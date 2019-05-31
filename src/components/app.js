6525ecb4e9f2c1f370c7e115e482d997e1dd28d1
Ammon Cisneros <ammon.15.2015.ci@gmail.com>
2019-05-31 13:35:48 -0700

    Init Commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import Profile from "./profile";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+      <div>
+        <Profile />
       </div>
     );
   }
