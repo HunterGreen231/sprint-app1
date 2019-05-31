58e6bd7d940647f975ec85fa58ea235568e931cf
g-hunter <webdev.huntergreen@gmail.com>
2019-05-31 09:38:22 -0600

    Initial commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>DevCamp React Starter</h1>
+        <h2>React Redux Router</h2>
+      </div>
+    );
+  }
+}
